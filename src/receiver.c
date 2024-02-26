ba6d11e16abc01ee7a6cd322145ae64e8c2dcd6e
chen-vv <vchen03@student.ubc.ca>
2024-02-26 15:53:55 -0800

    Wrote doxygen comments

--- src/receiver.c
+++ src/receiver.c
@@ -1,3 +1,19 @@
+/** @file receiver.c
+ *  @brief A UDP receiver program
+ *
+ *  This contains the code for the receiver
+ *  using the UDP file transfer protocol. The
+ *  main function parses the command line arguments 
+ *  and then calls the rrecv function to receive 
+ *  the file.
+ *
+ *  @author Vicky Chen (chen-vv)
+ *  @author Eric Omielan (eomielan)
+ *  @bug No known bugs.
+ */
+
+/* -- Includes -- */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,12 +27,35 @@
 #include <pthread.h>
 #include <errno.h>
 
+/** @brief Writes the bytes received on port myUDPport to a file 
+ *         called destinationFile at a rate of writeRate bytes 
+ *         per second.
+ *
+ *  If writeRate is 0 then the receiver can write as many bytes 
+ *  as possible into destinationFile. Otherwise, if writeRate is 
+ *  non-zero then the receiver should write no more than 
+ *  writeRate bytes per second to destinationFile. See rsend for 
+ *  the counterpart function.
+ *
+ *  @param myUDPport The port number to listen on.
+ *  @param destinationFile The name of the file to write to.
+ *  @param writeRate The maximum number of bytes to write per second.
+ *                   Must be an integer greater than or equal to zero.   
+ *  @return Void.
+ */
 void rrecv(unsigned short int myUDPport, 
             char* destinationFile, 
             unsigned long long int writeRate) {
 
 }
 
+
+/** @brief UDP receiver entrypoint.
+ *
+ *  TODO: Implement main & write comments
+ *
+ *  @return Should not return
+ */
 int main(int argc, char** argv) {
     // This is a skeleton of a main function.
     // You should implement this function more completely
--- src/sender.c
+++ src/sender.c
@@ -1,3 +1,19 @@
+/** @file sender.c
+ *  @brief A UDP sender program
+ *
+ *  This contains the code for the sender
+ *  using the UDP file transfer protocol. The
+ *  main function parses the command line arguments 
+ *  and then calls the rsend function to send 
+ *  the file.
+ *
+ *  @author Vicky Chen (chen-vv)
+ *  @author Eric Omielan (eomielan)
+ *  @bug No known bugs.
+ */
+
+/* -- Includes -- */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,6 +27,21 @@
 #include <pthread.h>
 #include <errno.h>
 
+/** @brief Sends the first bytesToTransfer bytes of the file
+ *         indicated by filename to the receiver at 
+ *         hostname:hostUDPport.
+ * 
+ *  This function sends the file using the UDP (SOCK_DGRAM) 
+ *  file transfer protocol. The bytes should be transferred
+ *  correctly and efficiently, even if the network drops or
+ *  reorders packets. See rrecv for the counterpart function.
+ *
+ *  @param hostname The name of the receiver host.
+ *  @param hostUDPport The port number on the receiver host.
+ *  @param filename The name of the file to transfer.
+ *  @param bytesToTransfer The number of bytes to transfer.
+ *  @return Void.
+ */
 void rsend(char* hostname, 
             unsigned short int hostUDPport, 
             char* filename, 
@@ -19,6 +50,14 @@ void rsend(char* hostname,
 
 }
 
+/** @brief UDP sender entrypoint.
+ *
+ *  TODO: Implement main & write comments
+ *  Mention that this takes in the command line arguments
+ *  for invoking the file transfer.
+ * 
+ * @return Should not return
+ */
 int main(int argc, char** argv) {
     // This is a skeleton of a main function.
     // You should implement this function more completely
