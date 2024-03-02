41d27e13405fc0247432e9325ff8be861d3c20f6
eomielan <eomielan@gmail.com>
2024-03-01 17:22:31 -0800

    Add sender track bandwidth test

---
+++ src/test/test_sender_bandwidth.py
@@ -0,0 +1,34 @@
+"""Run this script to measure the bandwidth usage of the sender."""
+
+import os
+import subprocess
+import time
+
+
+def measure_bandwidth():
+    start = time.time()
+
+    sender_process = subprocess.Popen(
+        [
+            "./sender",
+            "europa.eomielan-194296.elec331sp2024.emulab.net",
+            "12345",
+            "src/test/output.txt",
+            "123",
+        ]
+    )
+    sender_process.wait()
+
+    end = time.time()
+
+    duration = end - start
+    bandwidth_in_bytes = os.path.getsize("src/test/output.txt")
+    bandwidth_in_bits = bandwidth_in_bytes * 8
+    bandwidth_in_mbps = (bandwidth_in_bits / duration) / (1024 * 1024)
+
+    return bandwidth_in_mbps, duration
+
+
+if __name__ == "__main__":
+    bandwidth, duration = measure_bandwidth()
+    print("Bandwidth usage over {:.10f} seconds: {:.2f} Mbps".format(duration, bandwidth))
